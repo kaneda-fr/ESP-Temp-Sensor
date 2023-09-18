5e62b28e17a1825a1477261c7846c1ba2bb9f2ca
Sebastien Lacoste-Seris <sebastien.lacoste-seris.net@devfactory.com>
2023-09-18 19:23:17 +0200

    initial commit

---
+++ esp8266_temp_sensor.ino
@@ -0,0 +1,182 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-mqtt-publish-ds18b20-arduino/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+#include <ArduinoJson.h>
+
+#define WIFI_SSID "<SSID>>"
+#define WIFI_PASSWORD "<password>"
+
+// Raspberri Pi Mosquitto MQTT Broker
+#define MQTT_HOST IPAddress(192, 168, 1, 1)
+// For a cloud MQTT broker, type the domain name
+//#define MQTT_HOST "example.com"
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topics
+#define MQTT_PUB_TEMP "esp/temperature"
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;          
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+// Temperature value
+float temp;
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+/*void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}*/
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  sensors.begin();
+  Serial.begin(115200);
+  Serial.println();
+  
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  //mqttClient.onSubscribe(onMqttSubscribe);
+  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  //mqttClient.setCredentials("REPlACE_WITH_YOUR_USER", "REPLACE_WITH_YOUR_PASSWORD");
+  
+  connectToWifi();
+
+  int deviceCount = 0;
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  int count=0;
+  // good for about 10 sensors
+  StaticJsonDocument<192> payload;
+  char output[512];
+
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New temperature readings
+    sensors.requestTemperatures(); 
+
+    payload.clear();
+    for (int deviceIndex =0; deviceIndex <sensors.getDeviceCount(); deviceIndex++ ){
+        DeviceAddress deviceAddress;
+        String dev_addr="";
+        String pub="";
+
+      Serial.print("Device Adress: ");
+      // Temperature in Celsius degrees
+      sensors.getAddress(deviceAddress, deviceIndex);
+
+      for (uint8_t i = 0; i < 8; i++)
+      {
+        if (deviceAddress[i] < 0x10) {
+          Serial.print("0");
+          dev_addr += String("0");
+        }
+        //Serial.print(deviceAddress[i], HEX);
+        dev_addr += String(deviceAddress[i], HEX);
+      }
+      Serial.print(dev_addr);
+      Serial.println();
+      temp = sensors.getTempCByIndex(deviceIndex);
+      // Temperature in Fahrenheit degrees
+      //temp = sensors.getTempFByIndex(0);
+    
+      // Publish an MQTT message on topic esp/temperature/sensorID
+      //pub.concat(MQTT_PUB_TEMP);
+      payload[dev_addr] = temp;
+    }
+  
+    serializeJson(payload, output, 512);
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, output);                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %s \n", output);
+
+  }
+}
